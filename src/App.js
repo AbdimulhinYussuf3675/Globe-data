005aab4e65b88236f2f0e537c0d5b39d22361ecf
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-15 10:29:48 +0300

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Homepage from 'components/routes/Home';
+import DetailsPage from 'components/routes/Details';
 
 function App() {
-  return <h1>Abdimuldin</h1>;
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/details/:id" element={<DetailsPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
