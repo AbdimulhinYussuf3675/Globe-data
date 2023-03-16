5200a1903711ac9c3fe0a0c44faf71c6eca8c36f
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-16 08:09:34 +0300

    Update search functionalties

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
+import { v4 as uuidv4 } from 'uuid';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useSelector, useDispatch } from 'react-redux';
 import getCountries from 'features/FetchAPI';
-import CountriesList from '../CountriesList';
+import Country from 'components/Country';
 
 const Homepage = () => {
   const dispatch = useDispatch();
   const countries = useSelector((state) => state.countries.countries);
 
-  const [search, setSearch] = useState('');
+  const [searchItem, setSearchItem] = useState('');
 
   useEffect(() => {
     if (countries.length === 0) {
       dispatch(getCountries());
     }
   }, [dispatch, countries.length]);
+
+  const newCountries = countries.filter(
+    (country) => country.name.toLowerCase().includes(searchItem.toLowerCase())
+      || country.region.toLowerCase().includes(searchItem.toLowerCase()),
+  );
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchItem(e.target.value);
+  };
 
   return (
     <div>
@@ -24,25 +35,35 @@ const Homepage = () => {
           <HomeIcon className="svg_icons" />
         </div>
         <div className="search">
-          {' '}
-          <div className="input">
-            <input
-              className="inputField"
-              placeholder="Search by name..."
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            />
-          </div>
-          {countries
-            .filter((country) => country.name.toLowerCase().includes(search.toLowerCase()))
-            .map((country) => (
-              <CountriesList key={country.id} country={country} />
-            ))}
+          <input
+            type="text"
+            name="searchItem"
+            placeholder="search for a country"
+            value={searchItem}
+            onChange={handleSearch}
+          />
         </div>
         <div className="logo">
           <SettingsIcon className="svg_icons" />
         </div>
       </header>
+      <div className="card-container">
+        {
+            searchItem.length
+              ? newCountries.map((country) => (
+                <Country
+                  key={uuidv4()}
+                  data={country}
+                />
+              ))
+              : countries.map((country) => (
+                <Country
+                  key={uuidv4()}
+                  data={country}
+                />
+              ))
+          }
+      </div>
     </div>
   );
 };
